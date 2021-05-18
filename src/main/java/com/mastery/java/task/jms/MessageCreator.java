b3b53d6e00fa7ff38fdfd624f555faa0aa9af248
Wizzard-sudo <kolia.makarevich.98@gmail.com>
2021-05-18 19:49:55 +0300

    add ActiveMQ, Listener and Receiver messgae

---
+++ src/main/java/com/mastery/java/task/jms/MessageCreator.java
@@ -0,0 +1,24 @@
+package com.mastery.java.task.jms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Queue;
+
+@Component
+public class MessageCreator implements CommandLineRunner {
+
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.jmsMessagingTemplate.convertAndSend(this.queue, "This example text provided MessageCreator");
+        System.out.println("Message has been put to queue by sender");
+    }
+}
